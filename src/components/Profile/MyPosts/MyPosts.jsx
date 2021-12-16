809153a965b4754e719a4962379c6e49bba44566
la-unicsoft <la@unicsoft.com>
2021-12-16 08:26:54 +0200

    some rewiev

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -4,7 +4,8 @@ import Post from "./Post/Post";
 
 
 const MyPosts = (props) => {
-    let postElements = props.posts.map(p => <Post message={p.message}/>);
+
+    let postElements = props.posts.posts.map((post, index) => <Post key={index} message={post.message}/>);
     let newPostElement = React.createRef();
 
     let onAddPost = () => {
