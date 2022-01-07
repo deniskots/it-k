52a1dcc689f4596f15654a71a8b2e0bf0505bebc
denisko <denis374@gmail.com>
2022-01-07 14:16:19 +0200

    add header component(login,auth/me)

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import classes from'./Header.module.css';
+import s from './Header.module.css';
+import {NavLink} from "react-router-dom";
 
-const Header = () => {
+
+const Header = (props) => {
   return <header className={ classes.header}>
   <img src="https://cdn.dribbble.com/users/60166/screenshots/13967952/media/9ea8447026fdcadf66196aa38fcc0207.jpg?compress=1&resize=400x300" alt={""}></img>
+    <div className={s.loginBlock}>
+      {props.isAuth ? props.login : <NavLink to={'/login'}>Login</NavLink> }
+    </div>
   </header>
 }
-export default Header
+export default Header;
